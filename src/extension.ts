15dc54c0a43732ad18268f0ff5a1da41b40a572d
Michael Kirschner <mjk.kirschner@gmail.com>
2019-11-24 14:52:26 -0500

    remove title

--- src/extension.ts
+++ src/extension.ts
@@ -89,7 +89,6 @@ function generateHTMLCanvas(): string {
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Cat Coding</title>
 
 	</head>
 	<body>	
